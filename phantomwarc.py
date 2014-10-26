7c94879ad949759d3ace64f0306a83e1b59369ff
dhamaniasad <dhamaniasad@gmail.com>
2014-10-26 09:02:07 +0530

    Revert "update"
    This reverts commit 1281230fdc21a554e0063bc63496b756d32a1c29.

--- phantomwarc.py
+++ phantomwarc.py
@@ -3,8 +3,8 @@ import socket
 import os
 import errno
 from selenium import webdriver
-from selenium.webdriver.common.proxy import *
-import threading
+from selenium.webdriver.common.proxy import ProxyType
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 BASE_DIR = os.getcwd()
 WARC_DIR = BASE_DIR + "/warc/"
 CERT_PATH = BASE_DIR + "warcprox-ca"
@@ -23,20 +23,27 @@ sock.bind(('', 0))
 
 warcprox_port = sock.getsockname()[1]
 sock.close()
-PROX_ADDR = "localhost:%s" % warcprox_port
+PROX_ADDR = "127.0.0.1:%s" % warcprox_port
 
 
 def initiate_warcprox(port):
-    subprocess.Popen(["warcprox", "-p %s" % (warcprox_port), "-nwarc", "-z",
-                      "-d%s" % (WARC_DIR), "-j/dev/null"])
+    subprocess.call(["warcprox", "-p %s" % (warcprox_port), "-n warc", "-z",
+                    "-d%s" % (WARC_DIR)])
 initiate_warcprox(warcprox_port)
 
 
-def start_browsing():
-    service_args = [
-        '--proxy=%s' % PROX_ADDR,
-        '--proxy-type=http, https',
-    ]
-    driver = webdriver.PhantomJS(service_args=service_args)
-    driver.get("https://www.google.com")
-start_browsing()
+def start_browser(url):
+    desired_capabilities = dict(DesiredCapabilities.PHANTOMJS)
+    desired_capabilities["proxy"] = {"proxyType": ProxyType.MANUAL,
+                                     "sslProxy": PROX_ADDR,
+                                     "httpProxy": PROX_ADDR}
+    browser = webdriver.PhantomJS(
+        desired_capabilities=desired_capabilities,
+        service_args=[
+            "--proxy=%s" % PROX_ADDR,
+            "--ssl-certificates-path=%s" % CERT_PATH,
+            "--ignore-ssl-errors=true"])
+    browser.implicitly_wait(2)
+    browser.set_page_load_timeout(30)
+    return browser
+start_browser("https://www.google.com")
